47bb46673b6a73183ca3609c66cb93f8fce46801
Mahesh palaka <palakamahesh48325@gmail.com>
2024-11-22 09:01:32 +0530

    Update bidirectional_reg.v

--- Bidirectional Shift Register/bidirectional_reg.v
+++ Bidirectional Shift Register/bidirectional_reg.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
 
 //Author Name : Mahesh Palaka
-//Project Name: Parallel-in-Parallel-out shift Register
-
-module pipo_reg(clk,clear,d,q);
-
-  input clk,clear;
-
-  output reg [3:0]  q;
-  input [3:0] d;
-
-
-  always @(posedge clk )
-    begin
-      if (clear)
-        q <= 4'b0000;
-      else
-        q <= d;
-    end
-endmodule`timescale 1ns / 1ps
-
-//Author Name : Mahesh Palaka
 //Project Name: Bi-Driectional shift Register
 
 module bidirectional_reg  #(parameter MSB=4) (input d,                      // Declare input for data to the first flop in the shift register
